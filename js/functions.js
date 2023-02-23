44007219d7b654f5f1875484ca3ce8836d581476
Olga Peganova <volha25121987@gmail.com>
2023-02-23 11:20:25 +0700

    task 2 reworked

--- js/functions.js
+++ js/functions.js
@@ -1,24 +1,14 @@
 const checkStringLength = (string, length) => String(string).length <= length;
 
-console.log(checkStringLength('Непростой JS', 20));
-console.log(checkStringLength('12345678', 20));
-
-
 const isPalindrome = (string) => {
   string = String(string).toLowerCase().replaceAll(' ','');
   return string === string.split('').reverse().join('');
 };
 
-console.log(isPalindrome('radar'));
-console.log(isPalindrome('Лёша на полке клопа нашёл'));
-
-
 const extractNumber = (string) => {
   string = String(string).replace(/\D/g, '');
   return parseInt(string, 10);
 };
-
-console.log(extractNumber('2019 year'));
 
 const createNewString = (string, minLength, pad) => {
   while (string.length < minLength) {
@@ -26,5 +16,3 @@ const createNewString = (string, minLength, pad) => {
   }
   return string;
 };
-
-console.log(createNewString('7', 10, 'timepad'));
