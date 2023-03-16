fc8b1b84488039084a9aafa65569b796272d3ae0
Olga Peganova <volha25121987@gmail.com>
2023-03-16 23:02:34 +0700

    big pictures

--- js/fullPhotos.js
+++ js/fullPhotos.js
@@ -1,16 +1,20 @@
 import './sketch.js';
 
 const bigPicture = document.querySelector('.big-picture');
-const bigPictureImg = document.querySelector('.big-picture__img');
+const bigPictureImg = document.querySelector('.big-picture__img img');
+const bigPicturePreview = bigPicture.querySelector('.big-picture__preview');
 const picture = [...document.querySelectorAll('.picture__img')];
+const bigPictureLikes = bigPicture.querySelector('.likes-count');
+const bigPictureComments = bigPicture.querySelector('.comments-count');
+const bigPictureDescription = bigPicturePreview.querySelector('.social__caption');
 
 picture.map((element) =>
   element.addEventListener('click', (evt) => {
     const target = evt.target;
     bigPicture.classList.remove('hidden');
     bigPictureImg.querySelector('img').src = target.src;
-    bigPictureImg.querySelector('img').src = target.dataset.likes;
-    bigPictureImg.querySelector('img').src = target.dataset.commentscount;
+    bigPictureLikes.querySelector('.likes-count').src = target.dataset.likes;
+    bigPictureComments.querySelector('img').src = target.dataset.commentscount;
     console.log(target.dataset.likes);
     console.log(target.dataset.commentscount);
   }));
