66d72add2ebb97d5d8862d7e5d2fe8782c66f9c6
Olga Peganova <volha25121987@gmail.com>
2023-03-20 16:57:10 +0700

    8/2 почти готово

--- js/sketch.js
+++ js/sketch.js
@@ -1,24 +1,31 @@
 import {createPosts} from './data.js';
 import {openBigPicture} from './fullPhotos.js';
 
+//нашли фото
 const similarListTemplate = document.querySelector('#picture').content.querySelector('.picture');
+//нашли список фото
 const picturesList = document.querySelector('.pictures');
+//создаем элемент
 const picturesData = createPosts();
 
 const createPost = (data) => {
+  //клонируем
   const post = similarListTemplate.cloneNode(true);
+  //находим в разметке картинку
   const pictureImg = post.querySelector('.picture__img');
+  //меняем у него путь и описание
   pictureImg.src = data.url;
   pictureImg.alt = data.description;
+  //заполняем из data
   post.querySelector('.picture__comments').textContent = data.comments.length;
   post.querySelector('.picture__likes').textContent = data.likes;
-
+//вешаем обработчик и по клику открываем пост
   post.addEventListener('click', () => {
     openBigPicture(data);
   });
   return post;
 };
-
+//перебираем массив, на каждой итерации передаем в функцию, которая создает пост
 const renderPosts = () => {
   picturesData.forEach((item) => picturesList.append(createPost(item)));
 };
