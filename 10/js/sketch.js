91389de3ed443b418f5a707bef2ee389e8657e29
Keks <keks@htmlacademy.ru>
2023-03-30 11:35:01 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/sketch.js
@@ -0,0 +1,56 @@
+import {openBigPicture} from './full-Photos.js';
+import { getData } from './api.js';
+
+const GET_URL = 'https://28.javascript.pages.academy/kekstagram/data';
+const ERROR_TIMEOUT = 10000;
+//нашли фото
+const similarListTemplate = document.querySelector('#picture').content.querySelector('.picture');
+//нашли список фото
+const picturesList = document.querySelector('.pictures');
+
+const createPost = (data) => {
+  //клонируем
+  const post = similarListTemplate.cloneNode(true);
+  //находим в разметке картинку
+  const pictureImg = post.querySelector('.picture__img');
+  //меняем у него путь и описание
+  pictureImg.src = data.url;
+  pictureImg.alt = data.description;
+  //заполняем из data
+  post.querySelector('.picture__comments').textContent = data.comments.length;
+  post.querySelector('.picture__likes').textContent = data.likes;
+  //вешаем обработчик и по клику открываем пост
+  post.addEventListener('click', () => {
+    openBigPicture(data);
+  });
+  return post;
+};
+//перебираем массив, на каждой итерации передаем в функцию, которая создает пост,вставляем объект в picturesList
+const renderPosts = (data) => {
+  data.forEach((item) => picturesList.append(createPost(item)));
+};
+
+const onGetSuccess = (data) => renderPosts(data);
+
+const onGetFail = () => {
+  const errorBlock = document.createElement('div');
+  errorBlock.style.position = 'fixed';
+  errorBlock.style.top = '0';
+  errorBlock.style.left = '0';
+  errorBlock.style.height = '100%';
+  errorBlock.style.width = '100%';
+  errorBlock.style.color = 'yellow';
+  errorBlock.style.backgroundColor = 'blue';
+  errorBlock.style.textAlign = 'center';
+  errorBlock.textContent = 'Произошла ошибка загрузки';
+  errorBlock.style.padding = '30px';
+  document.body.append(errorBlock);
+
+  setTimeout(() => {
+    errorBlock.remove();
+  }, ERROR_TIMEOUT);
+};
+
+const getPicturesData = () => getData(GET_URL, onGetSuccess, onGetFail);
+
+export {getPicturesData};
